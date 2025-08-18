6b9a50a35233e018a56c2c271efe320da1ec34f6
Hasan AbouMinyar <among.00.12@gmail.com>
2025-08-18 18:02:08 +0200

    edit App main

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="relative min-h-screen lg:flex">
         <Sidebar />
-        <HomeView />
+        <main id="content" class="flex-1 pb-12 overflow-y-auto bg-gray-100 lg:h-screen">
+            <Navigation />
+            <router-view />
+        </main>
     </div>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar.vue';
-import HomeView from './components/HomeView.vue';
+import Navigation from './components/layout/Navigation.vue';
+import Sidebar from './components/layout/Sidebar.vue';
 
-export default {
-    components: {
-        Sidebar,
-        HomeView,
-    },
-};
+export default { components: { Sidebar, Navigation } };
 </script>
