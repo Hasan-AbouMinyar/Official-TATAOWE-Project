7808e480a596b7bc9e959d28f986d928b262fcc8
Hasan AbouMinyar <among.00.12@gmail.com>
2025-08-18 18:02:19 +0200

    create the new dash

---
+++ Frontend/src/components/dashboard/DashboardHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="flex flex-col w-full px-6 md:justify-between md:items-center md:flex-row">
+    <div>
+      <h2 class="text-3xl font-medium text-gray-800">Dashboard</h2>
+      <p class="mt-2 text-sm text-gray-500">Tatawoe Platform connects volunteers with charitable organizations, empowering communities through meaningful opportunitie</p>
+    </div>
+  </section>
+</template>
+<script setup>
+// presentational only
+</script>
