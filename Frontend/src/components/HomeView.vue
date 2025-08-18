1f00cc677c86146250633eaecbe5058c87e95688
Hasan AbouMinyar <among.00.12@gmail.com>
2025-08-18 16:28:53 +0200

    edit home

---
+++ Frontend/src/components/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main id="content" class="flex-1 pb-12 overflow-y-auto bg-gray-100 lg:h-screen">
+    <Navigation />
+    <DashboardHeader />
+    <Feed />
+  </main>
+</template>
+
+<script setup>
+import Navigation from './Navigation.vue';
+import DashboardHeader from './DashboardHeader.vue';
+import Feed from './Feed.vue';
+</script>
