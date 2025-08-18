8eeae1bcf705afa3acd3df827edca6bd11babf40
Hasan AbouMinyar <among.00.12@gmail.com>
2025-08-18 18:01:06 +0200

    add routes to the system with vite @ route

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import "./assets/app.css";
-
-createApp(App).mount('#app');
+import './assets/app.css';
+import router from './router';
+// Bootstrapping Vue app with router
+createApp(App).use(router).mount('#app');
