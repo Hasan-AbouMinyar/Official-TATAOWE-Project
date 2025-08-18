a10b55ecf46d3532e75ff8a37f853631dad93f2f
Hasan AbouMinyar <among.00.12@gmail.com>
2025-08-18 18:03:49 +0200

    the index dash

---
+++ Frontend/src/views/Dashboard/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <DashboardHeader />
+  <Feed />
+</template>
+<script setup>
+import DashboardHeader from '@/components/dashboard/DashboardHeader.vue';
+import Feed from '@/components/dashboard/feed/Feed.vue';
+</script>
